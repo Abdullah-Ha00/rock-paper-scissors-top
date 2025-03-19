fcd52e2e260a33e78f219da922193984c82f6365
Abdullah-Ha00 <abdallahhamze982@gmail.com>
2025-03-19 21:18:10 +0200

    Write computer random choice func

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("This is working!")
+function getComputerChoice() {
+    choices = "rock, paper, scissors"
+random = Math.random() * 10
+if (random <5) {
+   return answer= choices.slice(0,4)
+} else if (random< 7) {
+    return nanswer=choices.slice(6,11)
+} else {
+    return answer = choices.slice(13)
+}
+}
